1e00d766221e22e46c11a1d91590957159f12515
ximing766 <ximing766@gmail.com>
2025-05-27 12:50:47 +0800

    init website

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # my-project-doc
-Test
+Test my project doc
---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello from my-project-doc!")
+
+def my_function():
+    return 42
+
+
+if __name__ == "__main__":
+    main()
